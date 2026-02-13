3d4199ff785239a3bf564e174cb75bb2d271ac8d
Haneef <haneefrko@gmail.com>
2026-02-13 14:03:31 -0500

    register

--- server/frontend/src/App.js
+++ server/frontend/src/App.js
@@ -1,11 +1,17 @@
-import LoginPanel from "./components/Login/Login"
+import LoginPanel from "./components/Login/Login";
+import Register from "./components/Register/Register";
+import Dealers from "./components/Dealers/Dealers";
 import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <Routes>
+      <Route path="/" element={<Dealers />} />
+      <Route path="/dealers" element={<Dealers />} />
       <Route path="/login" element={<LoginPanel />} />
+      <Route path="/register" element={<Register />} />
     </Routes>
   );
 }
+
 export default App;
