012761fd97026aa7999214bf855c09ae5f51e6e4
haneef <haneefrko@gmail.com>
2026-02-13 20:11:20 -0500

    final

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -5,9 +5,9 @@ from . import views
 
 app_name = 'djangoapp'
 urlpatterns = [
-    path('login/', views.login_user, name='login'),
-    path('logout/', views.logout_request, name='logout'),
-    path('register/', views.registration, name='register'),
+    path('login', views.login_user, name='login'),
+    path('logout', views.logout_request, name='logout'),
+    path('register', views.registration, name='register'),
     path('get_cars/', views.get_cars, name='getcars'),
     path('get_dealers/', views.get_dealerships, name='get_dealers'),
     path('get_dealers/<str:state>/', views.get_dealerships, name='get_dealers_by_state'),
--- server/frontend/src/App.js
+++ server/frontend/src/App.js
@@ -2,15 +2,16 @@ import LoginPanel from "./components/Login/Login";
 import Register from "./components/Register/Register";
 import Dealers from "./components/Dealers/Dealers";
 import { Routes, Route } from "react-router-dom";
+import PostReview from "./components/Dealers/PostReview"
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Dealers />} />
-      <Route path="/dealers" element={<Dealers />} />
       <Route path="/login" element={<LoginPanel />} />
       <Route path="/register" element={<Register />} />
       <Route path="/dealers" element={<Dealers/>} />
+      <Route path="/postreview/:id" element={<PostReview/>} />
     </Routes>
   );
 }
